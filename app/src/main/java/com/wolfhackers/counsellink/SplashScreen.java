9fad32aebfef3896f4263f80dcb3c40a3b8709ce
Danish Suri <dsuri@ncsu.edu>
2016-10-13 23:47:52 -0400

    Final Commit

---
+++ app/src/main/java/com/wolfhackers/counsellink/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.wolfhackers.counsellink;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashScreen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        ImageView logo=(ImageView)findViewById(R.id.logo);
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Home.class));
+            }
+        });
+    }
+
+
+}
